b7034678def1c333de5daac7d6b939c4fb2ae82a
noshpineapple28 <manoucherin@gmail.com>
2025-03-23 17:32:17 -0400

    fix: set server process port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const socketIO = require("socket.io");
 const http = require("http");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // servers
 const server = http.createServer(app);
